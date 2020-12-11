af81259df31acbedb57aee7c25e2e5c8437350ae
Conway <hconway@luc.edu>
2020-12-10 20:38:24 -0600

    completed part 5

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -22,4 +22,9 @@ export class CrewComponent implements OnInit {
     this.crew.push({name: memberName, firstMission: isFirst});
   }
 
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
 }
